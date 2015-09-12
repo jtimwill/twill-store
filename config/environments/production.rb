202f05d6e7596ce48d9fa8233a1a499c8cbb9d49
Tim Williams <jtimwill@gmail.com>
2015-09-12 14:24:48 -0400

    modified email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,17 +19,13 @@ Rails.application.configure do
 
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { host: 'twill-mystore.herokuapp.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              ENV['MAILGUN_SMTP_SERVER'],
-  #   port:                 ENV['MAILGUN_SMTP_PORT'],
-  #   domain:               'twill-mystore.herokuapp.com',
-  #   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-  #   password:             ENV['MAILGUN_SMTP_PASSWORD'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  }
-
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'localhost:3000'}
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:               'twill-mystore.herokuapp.com',
+    authentication:       'plain',
+  }
 end
