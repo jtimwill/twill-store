d802c4a56037151bb122ebb3340524d667919aa9
Tim Williams <jtimwill@gmail.com>
2015-09-12 14:03:28 -0400

    stopped using mailgun for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,13 +19,17 @@ Rails.application.configure do
 
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = { host: 'twill-mystore.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              ENV['MAILGUN_SMTP_SERVER'],
-    port:                 ENV['MAILGUN_SMTP_PORT'],
-    domain:               'twill-mystore.herokuapp.com',
-    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
-    password:             ENV['MAILGUN_SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              ENV['MAILGUN_SMTP_SERVER'],
+  #   port:                 ENV['MAILGUN_SMTP_PORT'],
+  #   domain:               'twill-mystore.herokuapp.com',
+  #   user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+  #   password:             ENV['MAILGUN_SMTP_PASSWORD'],
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true  }
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.delivery_method = :letter_opener
 end
