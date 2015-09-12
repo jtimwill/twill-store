6607abfe143016cb0b070ab741182a0b8828a52c
Tim Williams <jtimwill@gmail.com>
2015-09-12 16:35:51 -0400

    updated seed file, added better dummy images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+large_images = ["/tmp/dummy-665x375-Comb.jpg",
+                "/tmp/dummy-665x375-Glass.jpg",
+                "/tmp/dummy-665x375-Geometry.jpg",
+                "/tmp/dummy-665x375-Stones.jpg",
+                "/tmp/dummy-665x375-White.jpg"]
 
-cars = Category.create(name: "Cars")
-planes = Category.create(name: "Planes")
+small_images = ["dummy-175x175-Bottles.jpg",
+                "dummy-175x175-Comb.jpg",
+                "dummy-175x175-DesiccationCracks.jpg",
+                "dummy-175x175-Glass.jpg",
+                "dummy-175x175-Geometry.jpg",
+                "dummy-175x175-RedDots.jpg",
+                "dummy-175x175-Stones.jpg",
+                "dummy-175x175-Utrecht.jpg",
+                "dummy-175x175-White.jpg"]
+
+odds = Category.create(name: "Odds")
+ends = Category.create(name: "Ends")
 
 10.times do |index|
-  Product.create(title: "Car #{index + 1}",
-               description: "Just a little description of Car #{index + 1}",
-               small_image_path: "/tmp/family_guy.jpg",
-               large_image_path: "http://dummyimage.com/665x375/000000/00a2ff",
+  Product.create(title: "Odd #{index + 1}",
+               description: "Just a little description of Odd #{index + 1}",
+               small_image_path: small_images.sample,
+               large_image_path1: large_images.sample,
+               large_image_path2: large_images.sample,
+               large_image_path3: large_images.sample,
                price: rand(1999..9999),
-               category_id: cars.id,
-               category: cars.name)
+               category_id: odds.id,
+               category: odds.name)
 end
 
 10.times do |index|
-  Product.create(title: "Plane #{index + 1}",
-               description: "Just a little description of Plane #{index + 1}",
-               small_image_path: "/tmp/futurama.jpg",
-               large_image_path: "http://dummyimage.com/665x375/000000/00a2ff",
+  Product.create(title: "End #{index + 1}",
+               description: "Just a little description of End #{index + 1}",
+               small_image_path: small_images.sample,
+               large_image_path1: large_images.sample,
+               large_image_path2: large_images.sample,
+               large_image_path3: large_images.sample,
                price: rand(1999..9999),
-               category_id: planes.id,
-               category: planes.name)
+               category_id: ends.id,
+               category: ends.name)
 end
 
 ShippingOption.create(title: "Standard Shipping (4-5 business days)", cost: 799)
