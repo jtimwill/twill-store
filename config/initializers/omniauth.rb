d0ffbe709ed50de744eb6a9e42dffd0c7a312c72
Tim Williams <jtimwill@gmail.com>
2015-09-23 11:12:04 -0400

    updated facebook initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  {
+    :scope => "email",
+    :info_fields=> "email"
+  }
 
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"],
     {
