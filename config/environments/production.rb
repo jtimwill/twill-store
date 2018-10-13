cb6bd0aeb871293a105952c25c1c292360f09dc1
Tim Williams <jtimwill@gmail.com>
2018-10-13 12:53:30 -0400

    Update production and staging mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     address:              ENV['MAILGUN_SMTP_SERVER'],
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:               'heroku.com',
+    domain:               ENV['PRODUCTION_DOMAIN'],
     authentication:       'plain',
     enable_starttls_auto:  'true'
   }
